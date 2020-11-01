87c2bb7150ce81b1904c528fdcde37d2c32fba3d
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-11-01 18:29:39 +0600

    signin goes to homescreen

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -2,40 +2,49 @@ import React, {useState} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import { Button, Input, Card} from "react-native-elements"
 import { MaterialCommunityIcons, FontAwesome5, AntDesign } from '@expo/vector-icons';
+import {AuthContext} from "../provider/AuthProvider"
 
 function SignInScreen(props) {
     return (
-        <View style={styles.viewStyle}>
-            <Card>
-                <Card.Title>Welcome to AuthApp!</Card.Title>
-                <Card.Divider/>
-                <Input 
-                    placeholder='E-mail Address'
-                    leftIcon={<MaterialCommunityIcons name="email-edit" size={24} color="black" />}
-                />
+        <AuthContext.Consumer>
+            {(auth) => (
+                <View style={styles.viewStyle}>
+                    <Card>
+                        <Card.Title>Welcome to AuthApp!</Card.Title>
+                        <Card.Divider/>
+                        <Input 
+                            placeholder='E-mail Address'
+                            leftIcon={<MaterialCommunityIcons name="email-edit" size={24} color="black" />}
+                        />
 
-                <Input
-                    placeholder='Password'
-                    leftIcon={<FontAwesome5 name="key" size={24} color="black" />}
-                    secureTextEntry={true}
-                />
+                        <Input
+                            placeholder='Password'
+                            leftIcon={<FontAwesome5 name="key" size={24} color="black" />}
+                            secureTextEntry={true}
+                        />
 
-                <Button
-                    icon = {<AntDesign name="login" size={24} color="white" />}
-                    title = '  Sign In'
-                    type = 'solid'
-                />
+                        <Button
+                            icon = {<AntDesign name="login" size={24} color="white" />}
+                            title = '  Sign In'
+                            type = 'solid'
+                            onPress = {
+                                function () {
+                                    auth.setIsloggedIn(true)
+                            }}
+                        />
 
-                <Button
-                    type='clear'
-                    icon={<AntDesign name="user" size={24} color="dodgerblue" />}
-                    title="Don't have an account?"
-                    onPress= {function () {
-                        props.navigation.navigate("SignUpScreen")
-                    }}
-                />
-            </Card>
-        </View>
+                        <Button
+                            type='clear'
+                            icon={<AntDesign name="user" size={24} color="dodgerblue" />}
+                            title="Don't have an account?"
+                            onPress= {function () {
+                                props.navigation.navigate("SignUpScreen")
+                            }}
+                        />
+                    </Card>
+                </View>)
+            }
+        </AuthContext.Consumer>
     )
 }
 
