aa2a0698a9e509fddba98e8af889170f42897981
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-11-01 23:07:40 +0600

    home and notification tabs added

--- App.js
+++ App.js
@@ -35,8 +35,34 @@ function HomeTabScreen() {
   return (
     <HomeTab.Navigation>
       <HomeTab.Screen name="Home" component={HomeScreen}
+        options={{
+        tabBarLabel: "Home",
+        tabBarIcon: ({ focused }) =>
+          focused ? (
+            <Entypo name="home" color="white" size={26} />
+          ) : (
+            <AntDesign name="home" color="white" size={22} />
+          ),
+      }}
       />
       <HomeTab.Screen name='Notification' component={NotificationScreen} />
+      <HomeTab.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{
+          tabBarLabel: "Notifications",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Ionicons name="ios-notifications" size={26} color="white" />
+            ) : (
+              <Ionicons
+                name="ios-notifications-outline"
+                size={22}
+                color="white"
+              />
+            ),
+        }}
+      />
     </HomeTab.Navigation>
   )
 }
