25139de927faf55719b90fdb9f6b4c2a1ec42f90
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-11-01 19:29:42 +0600

    Update SignInScreen.js

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -3,8 +3,13 @@ import {View, Text, StyleSheet} from 'react-native'
 import { Button, Input, Card} from "react-native-elements"
 import { MaterialCommunityIcons, FontAwesome5, AntDesign } from '@expo/vector-icons';
 import {AuthContext} from "../provider/AuthProvider"
+import {getDataJSON} from "../functions/AsyncStorage"
 
 function SignInScreen(props) {
+    const [Email, setEmail] = useState('');
+    const [Password, setPassword] = useState('');
+
+
     return (
         <AuthContext.Consumer>
             {(auth) => (
@@ -15,12 +20,18 @@ function SignInScreen(props) {
                         <Input 
                             placeholder='E-mail Address'
                             leftIcon={<MaterialCommunityIcons name="email-edit" size={24} color="black" />}
+                            onChangeText = {function (currentInput) {
+                                setEmail(currentInput);
+                            }}
                         />
 
                         <Input
                             placeholder='Password'
                             leftIcon={<FontAwesome5 name="key" size={24} color="black" />}
                             secureTextEntry={true}
+                            onChangeText = {function (currentInput) {
+                                setPassword(currentInput);
+                            }}
                         />
 
                         <Button
@@ -29,7 +40,14 @@ function SignInScreen(props) {
                             type = 'solid'
                             onPress = {
                                 function () {
-                                    auth.setIsLoggedIn(true)
+                                    let UserData = getDataJSON(Email);
+                                    if (UserData.password == Password) {
+                                        auth.setIsLoggedIn(true)
+                                        auth.setCurrentUser(UserData)
+                                    } else {
+                                        alert("LOGIN FAILED")
+                                    }
+                                    console.log(UserData)
                             }}
                         />
 
